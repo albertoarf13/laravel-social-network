80f0765116947ef5eb71ef9725e3ddd87ccd5ef3
albertoarf13 <54405329+albertoarf13@users.noreply.github.com>
2020-08-09 01:51:26 +0200

    create_profiles_table

---
+++ database/migrations/2020_08_08_233535_create_profiles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->mediumText('bio');
+            $table->string('website', 100);
+            $table->string('instagram', 100);
+            $table->string('pais', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
